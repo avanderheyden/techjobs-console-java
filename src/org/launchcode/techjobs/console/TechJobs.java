f4b482440ddbb4aac459a0932801a250510331a4
Amanda Vanderheyden <aev4882@gmail.com>
2019-11-17 20:07:31 -0600

    first step complete

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -11,7 +11,7 @@ public class TechJobs {
 
     private static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -103,7 +103,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -111,6 +111,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("Invalid entry. Try again");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("\n*****");
+
+                for (HashMap.Entry<String, String> jobInfo : job.entrySet()) {
+                    System.out.println(jobInfo.getKey() + ": " + jobInfo.getValue());
+//                System.out.println(jobInfo);
+                }
+
+            }
+        }
     }
 }
